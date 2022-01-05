c83f3a5df12dd6ac7e5fc584b1f7ecc760d5f30a
djleotechwizard <markvanschijndel13@gmail.com>
2022-01-05 20:10:21 +0100

    Added gMarioState

--- src/pc/server.c
+++ src/pc/server.c
@@ -1,7 +1,7 @@
 #include "httpd.h"
 #include <sys/stat.h>
 #include <pthread.h>
-
+#include <types.h>
 #define CHUNK_SIZE 1024 // read 1024 bytes at a time
 
 // Public directory settings
@@ -20,51 +20,16 @@ void initialize_server() {
     pthread_create(&pid, NULL, thread_function, NULL);
 }
 
-int file_exists(const char *file_name) {
-    struct stat buffer;
-    int exists;
+extern struct MarioState *gMarioState;
 
-    exists = (stat(file_name, &buffer) == 0);
-
-    return exists;
-}
-
-int read_file(const char *file_name) {
-    char buf[CHUNK_SIZE];
-    FILE *file;
-    size_t nread;
-    int err = 1;
-
-    file = fopen(file_name, "r");
-
-    if (file) {
-        while ((nread = fread(buf, 1, sizeof buf, file)) > 0)
-            fwrite(buf, 1, nread, stdout);
-
-        err = ferror(file);
-        fclose(file);
-    }
-    return err;
-}
 
 void route() {
     ROUTE_START()
 
-    // GET("/") {
-    //     char index_html[20];
-    //     sprintf(index_html, "%s%s", PUBLIC_DIR, INDEX_HTML);
-
-    //     HTTP_200;
-    //     if (file_exists(index_html)) {
-    //         read_file(index_html);
-    //     } else {
-    //         printf("Hello! You are using %s\n\n", request_header("User-Agent"));
-    //     }
-    // }
-
     GET("/testGET") {
         HTTP_200;
         printf("Test GET");
+        printf("%d", gMarioState->numCoins);
     }
 
     POST("/testPOST") {
@@ -74,21 +39,6 @@ void route() {
         if (payload_size > 0)
             printf("Request body: %s", payload);
     }
-
-    // GET(uri) {
-    //     char file_name[255];
-    //     sprintf(file_name, "%s%s", PUBLIC_DIR, uri);
-
-    //     if (file_exists(file_name)) {
-    //         HTTP_200;
-    //         read_file(file_name);
-    //     } else {
-    //         HTTP_404;
-    //         sprintf(file_name, "%s%s", PUBLIC_DIR, NOT_FOUND_HTML);
-    //         if (file_exists(file_name))
-    //             read_file(file_name);
-    //     }
-    // }
 
     ROUTE_END()
 }
